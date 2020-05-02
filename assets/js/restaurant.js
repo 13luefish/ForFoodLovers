f8b3922f7c3f233e11a0b21426e3670eb2d951d9
willhitch <hitch.will@gmail.com>
2020-05-02 13:02:22 -0600

    Yelp api running fully, tested append to page

--- assets/js/restaurant.js
+++ assets/js/restaurant.js
@@ -28,7 +28,36 @@ function getRestaurants() {
         Authorization: "Bearer " + yelpAPIKey,
       },
     }).then(function (response) {
-      console.log(response);
+      var result = response.businesses;
+
+      console.log(result);
+      for (var i = 0; i < result.length; i++) {
+        var newDiv = $("<div>");
+
+        var nameH1 = $("<h1>").text(result[i].name);
+        var img = $("<img/>").attr("src", result[i].image_url);
+        var price = $("<p>").text(result[i].price);
+        var rating = $("<p>").text(result[i].rating);
+        var address = $("<p>").text(
+          result[i].location.display_address.join(" ")
+        );
+        var link = $("<a>").text("Learn More").attr("href", result[i].url);
+
+        newDiv.append(nameH1, img, price, rating, address, link);
+
+        $("#restaurants").append(newDiv);
+      }
     });
   }
 }
+
+// function appendRestaurants(response) {
+//   console.log("this is running");
+//   for (var i = 0; i < response.length; i++) {
+//     console.log(response.businesses[i].name);
+//     console.log(response.businesses[i].image_url);
+//     console.log(response.businesses[i].price);
+//     console.log(response.businesses[i].rating);
+//     console.log(response.businesses[i].location);
+//   }
+// }
